f08e11dcf5c28701b38a71dcedc77253233f16a8
Lucas Aquino <70299812+Pochenke@users.noreply.github.com>
2022-12-22 22:08:46 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import path from "path";
 import ProductManager from "./ProductManager.js";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
